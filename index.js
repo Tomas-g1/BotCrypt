322d783e1d4d3a1a85eb1aff7a97887711ac8f83
Tomas-g1 <Soytomas1g@hotmail.com>
2025-10-29 15:28:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -129,9 +129,11 @@ client.on(Events.InteractionCreate, async (i) => {
   }
 });
 // registra los comandos automáticamente (solo dejar temporalmente)
+require('./deploy-commands');
 
 
 client.login(process.env.DISCORD_TOKEN);
+
 
 
 
